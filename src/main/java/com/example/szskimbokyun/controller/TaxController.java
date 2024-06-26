00fe966dceb92f5bac3471cbffef829793f43121
kimbokyun-EZ <kimbokyun@eztechfin.co.kr>
2024-06-26 16:56:44 +0900

    scrap

--- src/main/java/com/example/szskimbokyun/controller/TaxController.java
+++ src/main/java/com/example/szskimbokyun/controller/TaxController.java
@@ -1,6 +1,7 @@
 package com.example.szskimbokyun.controller;
 
 import com.example.szskimbokyun.service.MemberService;
+import com.example.szskimbokyun.service.TaxService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,12 @@ public class TaxController {
 
     private final MemberService memberService;
 
+    private final TaxService taxService; ;
 
     @Operation(summary = "스크래핑")
     @PostMapping("/scrap")
     public ResponseEntity scrap(){
-
+        taxService
 
         return ResponseEntity.ok("");
     }
--- src/main/java/com/example/szskimbokyun/service/TaxService.java
+++ src/main/java/com/example/szskimbokyun/service/TaxService.java
@@ -1,12 +1,30 @@
 package com.example.szskimbokyun.service;
 
+import com.example.szskimbokyun.util.HttpClientBase;
+import com.example.szskimbokyun.util.HttpHeaderUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class TaxService {
 
+    private final HttpClientBase httpClientBase;
+
+    public void scrap() throws URISyntaxException {
+        String SCRAP_URL = "https://codetest-v4.3o3.co.kr/scrap";
+        String AUTH_KEY = "fQL4PtoGRRgj6Q1OZG1WpQ==";
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, HttpHeaderUtil.setHeaderAuth(AUTH_KEY));
+        URI uri = new URI(SCRAP_URL);
+
+        ResponseEntity<String> stringResponseEntity = httpClientBase.callApi(String.valueOf(uri), HttpMethod.POST, requestEntity);
+    }
 
 }
